6783297e4da79a33f9fa48688b8ef5d8f0c33edf
Nghiệp Vo <kenghiep@ascvn.com.vn>
2018-05-21 15:03:58 +0700

    commit add admin lte
    commit add admin lte

--- app/app.module.js
+++ app/app.module.js
@@ -4,6 +4,7 @@ import '@progress/kendo-ui/css/web/kendo.bootstrap.css';
 import '@progress/kendo-ui/css/web/kendo.bootstrap.mobile.css';
 import 'angular-confirm1/css/angular-confirm.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'admin-lte/dist/css/adminlte.css';
 
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
@@ -11,7 +12,7 @@ import example from './example/example.module';
 import example2 from './example2/example2.module';
 import login from './login/login.module';
 import 'angular-confirm1';
-
+import 'admin-lte/dist/js/adminlte.js';
 
 angular.module('app', [
   uirouter, 'cp.ngConfirm', 'example', 'example2', 'login'
