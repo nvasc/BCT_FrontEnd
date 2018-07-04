1bdcc7f2568d238c493f0c3a439cac9b93de3c31
ttinos <40585774+ttinos@users.noreply.github.com>
2018-07-04 09:27:28 +0700

    Phân quyền ứng dụng cho tài khoản

--- app/controllers/giaodienchinh/quanly/taikhoan/taikhoan.controller.js
+++ app/controllers/giaodienchinh/quanly/taikhoan/taikhoan.controller.js
@@ -1,13 +1,19 @@
+import cacquyenungdungTemplate from './cacquyenungdungTemplete.html';
+import gridCommandTaiKhoan from './grid-command-taikhoan.html';
+import gridCommandCacQuyenUngDung from './grid-command-cacquyenungdung.html';
 
-import gridCommand from './grid-command.html';
 import colActive from './col-active.html';
-import saveTemplate from './save.html';
+import saveTaiKhoanTemplate from './save-taikhoan.html';
+import saveCacQuyenungDungTemplate from './save-cacquyenungdung.html';
+
 
 function taikhoanController ($q, $scope, taikhoanService, popupFactory) {
   const vm = this;
   // Message ------------------
   var rss = {
-    CreateTitle: 'Tạo mới tài khoản',
+    TitleTaiKhoan: 'tài khoản',
+    TitleQuyenTrongUngDung: 'quyền trong ứng dụng',
+
     CreateButton: 'Tạo mới',
     CreateButtonClass: 'btn-primary',
 
@@ -27,118 +33,117 @@ function taikhoanController ($q, $scope, taikhoanService, popupFactory) {
   // Initial in screen ------------------
   taikhoanService.init();
   var _scopeGrid = null;
+
   // Scope of Grid
   vm.setScopeGrid = function (s) {
     _scopeGrid = s;
   };
-
-  function refreshGrid() {
-    if (_scopeGrid) {
-      _scopeGrid.refresh();
-    }
-  }
-
-  // Column Define of Grid Component ------------------
-  vm.colDefs = [{
-    name: 'FullName',
-    displayName: 'Họ và tên',
-  }, {
-    name: 'Email',
-    displayName: 'Email',
-  },{
-    width: 90,
-    name: 'IsActive',
-    displayName: 'Trạng Thái',
-    cellTemplate: colActive,
-    cellClass: 'grid-text-align-center',
-    enableSorting: false,
-    dataType: 3
-  }, {
-    name: ' ',
-    cellTemplate: gridCommand,
-    cellClass: 'grid-command',
-    width: 60,
-    enableSorting: false,
-    enableFiltering: false,
-  }];
-
-  // manually Filter in Grid Component ------------------
-  vm.isFilter = false;
-  vm.filter = function () {
-    vm.isFilter = !vm.isFilter;
-  }
-
-  // CRUD for this function ------------------
-  vm.saveObj = {};
+  vm.defaultFilter = {};
   vm.action = {};
+  vm.saveObj = {};
+  vm.create = function (parentId, type, refreshGridCallBack) {
+    var title = '';
+    var template = '';
+    switch (type) {
+      case 1:
+        title = rss.CreateButton + ' ' + rss.TitleTaiKhoan;
+        template = saveTaiKhoanTemplate;
+        break;
+      case 2:
+        title = rss.CreateButton + ' ' + rss.TitleQuyenTrongUngDung;
+        template = saveCacQuyenungDungTemplate;
 
-  vm.create = function (id, type, refreshGridCallBack) {
+        vm.defaultFilter = {
+          UserId : parentId,
+          QueryId :0
+        };
+        break;
+    }
+
     popupFactory.setOptions({
       rss: rss,
-      title: rss.CreateTitle,
-      columnClass: 'col-md-offset-3 col-md-6',        
-      icon: 'fa fa-plus', 
-      content: saveTemplate,
+      title: title,
+      columnClass: 'col-md-offset-3 col-md-6',
+      icon: 'fa fa-plus',
+      content: template,
       scope: $scope,
     });
 
-    taikhoanService.get(0).then(function (obj) {      
+    taikhoanService.get(0, parentId, type).then(function (obj) {
       vm.saveObj = obj;
-      popupFactory.create(function () { 
+      vm.saveObj.IdUser = parentId;
+      vm.saveObj.userObj = { IdUser: parentId };
+      popupFactory.create(function () {
         var deferred = $q.defer();
-        taikhoanService.create(vm.saveObj).then(function () {
+        taikhoanService.create(type, vm.saveObj).then(function () {
           deferred.resolve(true);
-          if (_scopeGrid) {
-            _scopeGrid.grid.refresh();            
-          }  
+          if (refreshGridCallBack) {
+            refreshGridCallBack();
+          }
         }, function () {
-          deferred.resolve(false);  
-        })    
+          deferred.resolve(false);
+        })
         return deferred.promise;
-      }, function () { vm.saveObj = {}; });
-    });    
+      }, function () {
+        vm.saveObj = {};
+      });
+    });
   }
+  vm.action.create = vm.create;
 
   vm.update = function (row, type, refreshGridCallBack) {
     popupFactory.setOptions({
       rss: rss,
-      title: rss.UpdateTitle,
-      columnClass: 'col-md-offset-3 col-md-6',  
+      title: rss.UpdateTitle + ' ' + rss.TitleTaiKhoan,
+      columnClass: 'col-md-offset-3 col-md-6',
       icon: 'fa fa-edit',
-      content: saveTemplate,
+      content: saveTaiKhoanTemplate,
       scope: $scope,
     });
-    taikhoanService.get(row.entity.Id).then(function (obj) {
+
+    taikhoanService.get(row.entity.Id, 0, type).then(function (obj) {
       vm.saveObj = obj;
-      vm.saveObj.IsUpdate = true;
-      popupFactory.update(function () { 
+      popupFactory.update(function () {
         var deferred = $q.defer();
-        taikhoanService.update(row.entity.Id, vm.saveObj).then(function () {
+        taikhoanService.update(row.entity.Id, vm.saveObj, type)
+        .then(function () {
           deferred.resolve(true);
           if (refreshGridCallBack) {
             refreshGridCallBack();
-          }   
+          }
         }, function () {
-          deferred.resolve(false);  
-        })              
+          deferred.resolve(false);
+        })
         return deferred.promise;
-      }, function () { vm.saveObj = {}; });
+      }, function () {
+        vm.saveObj = {};
+      });
     });
-  };
+  }
   vm.action.update = vm.update;
 
   vm.delete = function (row, type, refreshGridCallBack) {
+    var title = '';
+    switch (type) {
+      case 1:
+        title = rss.DeleteButton + ' ' + rss.TitleTaiKhoan;
+        break;
+      case 2:
+        title = rss.DeleteButton + ' ' + rss.TitleQuyenTrongUngDung;
+        break;
+    }
     popupFactory.setOptions({
       rss: rss,
-      title: rss.DeleteTitle,
+      title: title,
       columnClass: 'col-md-offset-5 col-md-3',   
       icon: 'fa fa-times',
       content: rss.DeleteConfirm,
       scope: $scope,
     });
+
     popupFactory.delete(function () { 
       var deferred = $q.defer();
-      taikhoanService.delete(row.entity.Id).then(function () {
+      taikhoanService.delete(type, row.entity.Id).then(function () {
         deferred.resolve(true);
         if (refreshGridCallBack) {          
           refreshGridCallBack();
@@ -148,8 +153,77 @@ function taikhoanController ($q, $scope, taikhoanService, popupFactory) {
       })    
       return deferred.promise;
     }, function () { vm.saveObj = {} });
-  };
+  }
   vm.action.delete = vm.delete;
+
+  // Grid defined ------------------------
+  vm.quyenungdungExpand = {
+    filterDefault: {
+      IdUser : 0
+    },
+    action: {
+      create: vm.create,
+      delete: vm.delete
+    },
+    rowTemplate: cacquyenungdungTemplate,
+    height: 300,
+    rowExpandedStateChanged: function (row, event) {
+      this.filterDefault.IdUser = row.entity.Id;
+    },
+    create: function () {
+      console.log('zo');
+    },
+    Options: {
+      colDefs: [{
+        name: 'RoleName',
+        displayName: 'Tên quyền',
+      }, {
+        name: 'RoleDescription',
+        displayName: 'Mô tả',
+      },{
+        name: ' ',
+        cellTemplate: gridCommandCacQuyenUngDung,
+        cellClass: 'grid-command',
+        width: 60,
+        enableSorting: false,
+        enableFiltering: false,
+      }],
+    }
+  }
+
+  vm.colDefs = [{
+    name: 'FullName',
+    displayName: 'Họ và tên',
+  }, {
+    name: 'Email',
+    displayName: 'Email',
+  },{
+    name: 'IsActive',
+    displayName: 'Hoạt động',
+    width: 90,
+    cellTemplate: colActive,
+    cellClass: 'grid-text-align-center',
+    enableSorting: false,
+    dataType: 3
+  }, {
+    name: ' ',
+    cellTemplate: gridCommandTaiKhoan,
+    cellClass: 'grid-command',
+    width: 60,
+    enableSorting: false,
+    enableFiltering: false,
+  }];
+
+  //Default Filter 
+  vm.filterDefault = {
+    Level: 1,
+  }
+
+  // manually Filter in Grid Component ------------------
+  vm.isFilter = false;
+  vm.filter = function () {
+    vm.isFilter = !vm.isFilter;
+  }
 }
 
 /* @ngInject */
--- app/controllers/giaodienchinh/quanly/taikhoan/taikhoan.service.js
+++ app/controllers/giaodienchinh/quanly/taikhoan/taikhoan.service.js
@@ -1,5 +1,7 @@
 function taikhoanService($q, $rootScope, $timeout, nonceProvider, dataProvider) {
   var service = {};
+  var _providerTaiKhoan = 'taikhoan';
+  var _providerPhanQuyenNguoidung = 'userrole';
 
   var _init = function () {
     $timeout(function () {
@@ -28,9 +30,15 @@ function taikhoanService($q, $rootScope, $timeout, nonceProvider, dataProvider) 
   };
   service.get = _get;
 
-  var _create = function (obj) {
+  var _create = function (type, obj) {
+    var _provider = '';
+    if (type === 1) {
+      _provider = _providerTaiKhoan;
+    } else {
+      _provider = _providerPhanQuyenNguoidung + '/PostDataForTaiKhoan';
+    }
     var deferred = $q.defer();
-    var taikhoanProvider = dataProvider.provider('taikhoan', _getKey());
+    var taikhoanProvider = dataProvider.provider(_provider, _getKey());
     taikhoanProvider.create({}, obj, function (res) {
       deferred.resolve(res);
     });    
@@ -49,11 +57,18 @@ function taikhoanService($q, $rootScope, $timeout, nonceProvider, dataProvider) 
   };
   service.update = _update;
 
-  var _delete = function (id) {    
+  var _delete = function (type, id) {    
     var deferred = $q.defer();    
+    var _provider = '';
+    if (type === 1) {
+      _provider = _providerTaiKhoan;
+    } else {
+      _provider = _providerPhanQuyenNguoidung;
+    }
+
     nonceProvider.getForVAFT().then(function (key) {
       _setKey(key);
-      var taikhoanProvider = dataProvider.provider('taikhoan', _getKey());
+      var taikhoanProvider = dataProvider.provider(_provider, _getKey());
       taikhoanProvider.delete({ id: id }, function (res) {
         deferred.resolve(res);
       });
