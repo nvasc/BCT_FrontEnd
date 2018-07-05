67e18f800cdd3d19bb66caf7acf7cd0c5ba64714
ttinos <40585774+ttinos@users.noreply.github.com>
2018-07-05 17:52:01 +0700

    Bổ sung chức năng dịch vụ chạy ngầm

---
+++ app/controllers/giaodienchinh/quantri/dichvuchayngam/dichvuchayngam.component.js
@@ -0,0 +1,10 @@
+import './style.css';
+import dichvuchayngamHtml from './dichvuchayngam.html';
+import dichvuchayngamController from './dichvuchayngam.controller';
+/* @ngInject */
+let dichvuchayngamComponent = {
+  template: dichvuchayngamHtml,
+  controllerAs: 'dichvuchayngam',
+  controller: dichvuchayngamController
+}
+export default dichvuchayngamComponent;
