6b0f73d91a28fa8912a1971e6d3313ec93e41dec
navan123 <nguyenanhvan.net@gmail.com>
2018-07-11 16:46:09 +0700

    báo cáo lãnh đạo

--- app/controllers/giaodienchinh/cacbaocao/baocaolanhdao/baocaolanhdao.service.js
+++ app/controllers/giaodienchinh/cacbaocao/baocaolanhdao/baocaolanhdao.service.js
@@ -1,10 +1,35 @@
-function baocaolanhdaoService($rootScope, $timeout) {
+function baocaolanhdaoService($q, $rootScope, $timeout, nonceProvider, dataProvider) {
 
   var service = {};
   var _init = function () {
     $(window, '.content-wrapper').unbind('resize');
+    $timeout(function () {
+      $.AdminLTE.layout.fix();
+      
+    });
   };
   service.init = _init;
+
+  var _key = '';
+  var _getKey = function () {return _key;}
+  service.getKey = _getKey;
+  var _setKey = function (key) { _key = key;}
+  service.setKey = _setKey;
+
+  var _get = function (id) { 
+    var deferred = $q.defer();
+    nonceProvider.getForVAFT().then(function (key) {
+      _setKey(key);
+      var baocaolanhdaoProvider = dataProvider.provider('baocaolanhdao');
+      var obj = baocaolanhdaoProvider.get({id: id}, function () {
+        obj.ApplicationType = obj.ApplicationType + '';
+        deferred.resolve(obj);
+      });       
+    });
+    return deferred.promise;
+  };
+  service.get = _get;
+
   return service
 
 }
