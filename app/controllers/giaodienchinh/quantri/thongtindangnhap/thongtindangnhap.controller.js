b9abcd0f736b126c3194fbbfdadead47ce3fea50
ttinos <40585774+ttinos@users.noreply.github.com>
2018-07-16 11:34:30 +0700

    Thông tin đăng nhập

--- app/controllers/giaodienchinh/quantri/thongtindangnhap/thongtindangnhap.controller.js
+++ app/controllers/giaodienchinh/quantri/thongtindangnhap/thongtindangnhap.controller.js
@@ -1,6 +1,129 @@
-function thongtindangnhapController ($scope, thongtindangnhapService) {
+import gridCommand from './grid-command.html';
+import deleteTemplate from './deleteTemplate.html';
+
+function thongtindangnhapController($q, $scope, thongtindangnhapService, popupFactory) {
   const vm = this;
+  //Get Role
+  vm.role = thongtindangnhapService.getRole();
+  // Message ------------------
+  var rss = {
+    DeleteTitle: 'Yêu cầu đăng xuất',
+    DeleteButton: 'Đăng xuất',
+    DeleteButtonClass: 'btn-danger',
+    CancelButton: 'Hủy',
+    CancelButtonClass: 'btn-default',
+    ViewTitle: 'Chi tiết',
+    CloseButton: 'Đóng'   
+  }
+  // Initial in screen ------------------
   thongtindangnhapService.init();
+  var _scopeGrid = null;
+  // Scope of Grid
+  vm.setScopeGrid = function (s) {
+    _scopeGrid = s;
+  };
+
+  // Column Define of Grid Component ------------------
+  vm.colDefs = [{
+    name: 'Username',
+    displayName: 'Tài khoản',
+  }, {
+    name: 'UserAgent',
+    displayName: 'User Agent',
+    cellTooltip: function(row, col) {
+      return row.entity.IssuerFrom;
+    }
+  },{
+    name: 'PublicIp',
+    displayName: 'Địa chỉ IP',
+  }, {
+    name: 'Issuer',
+    displayName: 'Kết nối từ',
+  },{
+    name: 'IssuerFrom',
+    displayName: 'Kết nối đến',
+  },{
+    name: 'IssuedDate',
+    cellFilter: 'date:\'dd-MM-yyyy HH:mm\'',
+    displayName: 'Ngày đăng nhập',
+  },{
+    name: 'ExpiresDate',
+    cellFilter: 'date:\'dd-MM-yyyy HH:mm\'',
+    displayName: 'Ngày hết hạn',
+  }, {
+    name: ' ',
+    cellTemplate: gridCommand,
+    cellClass: 'grid-command',
+    width: 60,
+    enableSorting: false,
+    enableFiltering: false,
+  }];
+  // manually Filter in Grid Component ------------------
+  vm.isFilter = false;
+  vm.filter = function () {
+    vm.isFilter = !vm.isFilter;
+  }
+  
+  // CRUD for this function ------------------
+  
+  vm.saveObj = {};
+
+  vm.delete = function (row) {
+    var deletePopup = null;
+    var option = {
+      rss: rss,
+      title: vm.role.delete ? rss.DeleteTitle : rss.ViewTitle,
+      columnClass: 'col-md-offset-2 col-md-8',   
+      icon: 'fa fa-times',
+      content: deleteTemplate,
+      scope: $scope,      
+    }
+
+    if (!vm.role.delete) {
+      option.buttons = {
+        close: {
+          text: rss.CloseButton,
+          btnClass: rss.CancelButtonClass,
+          action: function (scope, button) {
+            
+          }
+        }
+      }
+    }
+    else {
+      option.buttons = {
+        Delete: {
+          text: rss.DeleteButton,
+          btnClass: rss.DeleteButtonClass,
+          action: function (scope, button) {
+            thongtindangnhapService.delete(row.entity.Id).then(function () {
+              if (_scopeGrid) {
+                _scopeGrid.grid.refresh();
+              }
+              if (deletePopup) {
+                deletePopup.close();
+              }
+            });     
+            
+            return false;
+          }
+        },
+        close: {
+          text: rss.CancelButton,
+          btnClass: rss.CancelButtonClass,
+          action: function (scope, button) {
+            
+          }
+        }
+      }
+    }
+    
+    deletePopup = popupFactory.setOptions(option);
+    thongtindangnhapService.get(row.entity.Id).then(function (obj) {  
+      vm.saveObj = obj;  
+      popupFactory.custom();
+    });
+  };  
 }
 
 /* @ngInject */
--- app/controllers/giaodienchinh/quantri/thongtindangnhap/thongtindangnhap.service.js
+++ app/controllers/giaodienchinh/quantri/thongtindangnhap/thongtindangnhap.service.js
@@ -1,14 +1,51 @@
-function thongtindangnhapService($state, $timeout) {
-
+function thongtindangnhapService($q, $rootScope, $timeout, nonceProvider, dataProvider, 
+  roleFactory) {  
   var service = {};
+  var _providerName = 'tokeninfor';
   var _init = function () {
     $(window, '.content-wrapper').unbind('resize');
     $timeout(function () {
       $.AdminLTE.layout.fix();
-      
-    });    
+    });
   };
+  var _key = '';
+  var _getKey = function () {return _key;}
+  service.getKey = _getKey;
+  var _setKey = function (key) { _key = key;}
+  service.setKey = _setKey;
   service.init = _init;
+
+  service.getRole = function () {
+    return roleFactory.getRoleFor('ttdn');
+  }
+
+  var _get = function (id) { 
+    var deferred = $q.defer();
+    nonceProvider.getForVAFT().then(function (key) {
+      _setKey(key);
+      var thongtindangnhapProvider = dataProvider.provider(_providerName);
+      var obj = thongtindangnhapProvider.get({id: id}, function () {
+        obj.ApplicationType = obj.ApplicationType + '';
+        deferred.resolve(obj);
+      });       
+    });
+    return deferred.promise;
+  };
+  service.get = _get;
+
+  var _delete = function (id) {    
+    var deferred = $q.defer();    
+    nonceProvider.getForVAFT().then(function (key) {
+      _setKey(key);
+      var thongtindangnhapProvider = dataProvider.provider(_providerName, _getKey());
+      thongtindangnhapProvider.delete({ id: id }, function (res) {
+        deferred.resolve(res);
+      });
+    });
+    return deferred.promise; 
+  };
+  service.delete = _delete;
+
   return service
 
 }
