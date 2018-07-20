4365f552bd7f42aaa3a6a5d9df69d24f1367b21c
Nghiệp Vo <kenghiep@ascvn.com.vn>
2018-07-20 08:42:52 +0700

    remove _isAllAccess and _isSystem

--- app/commons/authentication/oauthDataFactory.js
+++ app/commons/authentication/oauthDataFactory.js
@@ -106,24 +106,6 @@ function oauthDataFactory(localStorageService, $rootScope, $location) {
   };   
   dataFactory.removeRememberMe = _removeRememberMe;
 
-  var _isAllAccess = function () {
-    var tokenObj = _getToken();
-    if (tokenObj && tokenObj.sat.length === 2 && tokenObj.sat[0] === '1') {
-      return true;
-    }    
-    return false;
-  };
-  dataFactory.isAllAccess = _isAllAccess;
-
-  var _isSystem = function () {
-    var tokenObj = _getToken();
-    if (tokenObj && tokenObj.sat.length === 2 && tokenObj.sat[1] === '1') {
-      return true;
-    }    
-    return false;
-  };
-  dataFactory.isSystem = _isSystem;
-
   return dataFactory;
 }
 
